74704035d0383a2ecf09cf15314ae95e6d3afa87
shicks1080 <stuart.hicks@gmail.com>
2019-11-29 11:06:24 +0100

    Adding Deleting

--- src/index.js
+++ src/index.js
@@ -71,6 +71,22 @@ app.patch('/users/:id', async (req, res) => {
   }
 })
 
+app.delete('/users/:id', async (req, res) => {
+  const _id = req.params.id
+
+  try {
+    const user = await User.findByIdAndDelete(_id)
+
+    if (!user) {
+      return res.status(404).send(_id)
+    }
+
+    res.send(user)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
 
@@ -125,6 +141,21 @@ app.patch('/tasks/:id', async(req, res) => {
     res.send(task)
   } catch (e) {
     res.status(400).send(e)
+  }
+})
+
+app.delete('/tasks/:id', async(req, res) => {
+  const _id = req.params.id
+
+  try {
+    const task = await Task.findByIdAndDelete(_id)
+
+    if (!task) {
+      res.status(404).send(_id)
+    }
+    res.send(task)
+  } catch (e) {
+    res.status(500).send(e)
   }
 })
 
