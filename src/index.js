2f5483d140e0375e0760260a8c019514593c793a
shicks1080 <stuart.hicks@gmail.com>
2019-11-29 09:37:40 +0100

    Complete the async/await refactoring

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
 
   try {
-    await.task.save()
+    await task.save()
     res.status(201).send(task)
   } catch (e) {
     res.status(400).send(e)
@@ -61,14 +61,14 @@ app.post('/tasks', async (req, res) => {
 app.get('/tasks', async (req, res) => {
 
   try {
-    const tasks = Task.find({})
+    const tasks = await Task.find({})
     res.send(tasks)
   } catch (e) {
     res.status(500).send()
   }
 })
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
   const _id = req.params.id
 
   try {
