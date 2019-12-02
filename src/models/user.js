e3b94d2b1b2d96eb01c0282366331ae5575c256a
shicks1080 <stuart.hicks@gmail.com>
2019-12-02 14:45:37 +0100

    Adding authorisation
    Using jsonwebtokens

--- src/models/user.js
+++ src/models/user.js
@@ -51,7 +51,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.methods.generateAuthToken = async function () {
   const user = this
-  const token = jwt.sign({ _id: user._id.toString }, 'thisismynewcourse')
+  const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
 
   user.tokens = user.tokens.concat({ token })
   await user.save()
