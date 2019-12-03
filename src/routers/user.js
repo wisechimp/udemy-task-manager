997718a91afeaa28b0c1770bbd8042f9506f1c1f
shicks1080 <stuart.hicks@gmail.com>
2019-12-03 15:00:54 +0100

    A simple image upload
    Adding multer to enable image uploads. Just adding an image locally at the moment.

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = new express.Router()
+const multer = require('multer')
 
 const User = require('../models/user')
 const auth = require('../middleware/auth')
@@ -94,6 +95,14 @@ router.delete('/users/me', auth, async (req, res) => {
   } catch (e) {
     res.status(500).send('Eh what\'s up with deleting this?')
   }
+})
+
+const upload = multer({
+  dest: 'avatars'
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+  res.send()
 })
 
 module.exports = router
