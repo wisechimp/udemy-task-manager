e51b5a6f944739488982aff11e0832b7a403fcd5
shicks1080 <stuart.hicks@gmail.com>
2019-12-03 14:22:53 +0100

    Adding pagination
    Adding limit and skip

--- src/routers/task.js
+++ src/routers/task.js
@@ -32,7 +32,11 @@ router.get('/tasks', auth, async (req, res) => {
     //const tasks = await Task.find({ owner: req.user._id })
     await req.user.populate({
       path: 'tasks',
-      match
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip)
+      }
     }).execPopulate()
     res.send(req.user.tasks)
   } catch (e) {
